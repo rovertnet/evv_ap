b20d43ffe226fe81883de2710053c6f3643c2371
Robert Matundu <matundukabamba@gmail.com>
2024-10-08 20:41:16 +0100

    last commit

--- documents/src/components/TB/Count.jsx
+++ documents/src/components/TB/Count.jsx
@@ -1,6 +1,8 @@
 import { RiLuggageDepositFill } from "react-icons/ri";
 import { PiUsersThreeFill } from "react-icons/pi";
 import { LiaUserFriendsSolid } from "react-icons/lia";
+import { TbExternalLink } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 
 export default function Count() {
@@ -12,9 +14,17 @@ export default function Count() {
             <h2 className="font-bold text-xl text-blue-700">Départments</h2>
             <RiLuggageDepositFill className="text-4xl text-blue-800" />
           </div>
-          <span className="font-medium text-base text-gray-500">
+          <span className="font-medium text-base text-gray-500 pb-7">
             14 départements
           </span>
+          <div className="flex justify-between items-center pt-5">
+            <span className="font-medium text-base text-slate-500">
+              Voir tout
+            </span>
+            <Link to="/depart">
+              <TbExternalLink className="text-2xl text-gray-500" />
+            </Link>
+          </div>
         </div>
 
         <div className="w-96 h-32 border-l-4 border-green-600 rounded-md px-5 py-3 bg-white">
@@ -25,6 +35,14 @@ export default function Count() {
           <span className="font-medium text-base text-gray-500">
             10 000 membres
           </span>
+          <div className="flex justify-between items-center pt-5">
+            <span className="font-medium text-base text-slate-500">
+              Voir tout
+            </span>
+            <Link to="/membre">
+              <TbExternalLink className="text-2xl text-gray-500" />
+            </Link>
+          </div>
         </div>
 
         <div className="w-96 h-32 border-l-4 border-orange-700 rounded-md px-5 py-3 bg-white">
@@ -35,6 +53,14 @@ export default function Count() {
           <span className="font-medium text-base text-gray-500">
             100 serviteurs
           </span>
+          <div className="flex justify-between items-center pt-5">
+            <span className="font-medium text-base text-slate-500">
+              Voir tout
+            </span>
+            <Link to="/membre">
+              <TbExternalLink className="text-2xl text-gray-500" />
+            </Link>
+          </div>
         </div>
       </div>
     </>
