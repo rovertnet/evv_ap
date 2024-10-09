c7158ff3b454f2922abde87fe945f5e0512d0de6
Robert Matundu <matundukabamba@gmail.com>
2024-10-09 23:20:17 +0100

    last commit

--- documents/src/components/header/Header.jsx
+++ documents/src/components/header/Header.jsx
@@ -13,6 +13,7 @@ import PersonAdd from "@mui/icons-material/PersonAdd";
 import Settings from "@mui/icons-material/Settings";
 import Logout from "@mui/icons-material/Logout";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 
@@ -67,32 +68,32 @@ export default function Header() {
                   paper: {
                     elevation: 0,
                     sx: {
-                      overflow: 'visible',
-                      filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                      overflow: "visible",
+                      filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
                       mt: 1.5,
-                      '& .MuiAvatar-root': {
+                      "& .MuiAvatar-root": {
                         width: 32,
                         height: 32,
                         ml: -0.5,
                         mr: 1,
                       },
-                      '&::before': {
+                      "&::before": {
                         content: '""',
-                        display: 'block',
-                        position: 'absolute',
+                        display: "block",
+                        position: "absolute",
                         top: 0,
                         right: 14,
                         width: 10,
                         height: 10,
-                        bgcolor: 'background.paper',
-                        transform: 'translateY(-50%) rotate(45deg)',
+                        bgcolor: "background.paper",
+                        transform: "translateY(-50%) rotate(45deg)",
                         zIndex: 0,
                       },
                     },
                   },
                 }}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
               >
                 <MenuItem onClick={handleClose}>
                   <Avatar /> Profile
@@ -111,7 +112,7 @@ export default function Header() {
                   <ListItemIcon>
                     <Settings fontSize="small" />
                   </ListItemIcon>
-                  Parametre     
+                  <Link to="/settings">Parametre</Link>
                 </MenuItem>
                 <MenuItem onClick={handleClose}>
                   <ListItemIcon>
--- documents/src/pages/Docs.jsx
+++ documents/src/pages/Docs.jsx
@@ -1,9 +1,10 @@
+import EnhancedTable from "../components/docs/Table";
 
 export default function Docs() {
   return (
     <>
       <div className=" py-5 px-5">
-        Documents
+        <EnhancedTable />
       </div>
     </>
   );
