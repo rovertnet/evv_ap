4ac4a27989478d3d74a130d5732ee56a48ea3693
Robert Matundu <matundukabamba@gmail.com>
2024-10-10 00:15:08 +0100

    last commit

--- documents/src/pages/Membre.jsx
+++ documents/src/pages/Membre.jsx
@@ -1,7 +1,10 @@
+import Membres from "../components/members/Membre"
 
 
 export default function Membre() {
   return (
-    <div className=" py-5 px-5">Membre</div>
+    <div className=" py-5 px-5">
+      <Membres />
+    </div>
   );
 }
