53bd400103143a64c1a0054fc7e3912c13f5b739
Robert Matundu <matundukabamba@gmail.com>
2024-10-10 19:52:02 +0100

    another commit

--- documents/src/components/depart/DepartTabs.jsx
+++ documents/src/components/depart/DepartTabs.jsx
@@ -62,23 +62,23 @@ export default function VerticalTabs() {
         onChange={handleChange}
         aria-label="Vertical tabs example"
         sx={{ borderRight: 1, borderColor: "divider" }}
-        className="w-48"
+        className="w-48 tabs-header"
       >
-        <Tab label="Pastoral" {...a11yProps(0)} className="text-left" />
-        <Tab label="Culte" {...a11yProps(1)} className="text-left" />
-        <Tab label="Programme" {...a11yProps(2)} className="text-left" />
-        <Tab label="Communication" {...a11yProps(3)} className="text-left" />
-        <Tab label="Social" {...a11yProps(4)} className="text-left" />
-        <Tab label="Couple" {...a11yProps(5)} className="text-left" />
-        <Tab label="Mamans" {...a11yProps(6)} className="text-left" />
-        <Tab label="Ecodim" {...a11yProps(6)} className="text-left" />
-        <Tab label="Intercession" {...a11yProps(6)} className="text-left" />
-        <Tab label="Evangelisation" {...a11yProps(6)} className="text-left" />
-        <Tab label="Technique" {...a11yProps(6)} className="text-left" />
-        <Tab label="Décoration" {...a11yProps(6)} className="text-left" />
-        <Tab label="Nettoyage" {...a11yProps(6)} className="text-left" />
-        <Tab label="Protocole" {...a11yProps(6)} className="text-left" />
-        <Tab label="Chorale" {...a11yProps(6)} className="text-left" />
+        <Tab label="Pastoral" {...a11yProps(0)} />
+        <Tab label="Culte" {...a11yProps(1)} />
+        <Tab label="Programme" {...a11yProps(2)} />
+        <Tab label="Communication" {...a11yProps(3)} />
+        <Tab label="Social" {...a11yProps(4)} />
+        <Tab label="Couple" {...a11yProps(5)} />
+        <Tab label="Mamans" {...a11yProps(6)} />
+        <Tab label="Ecodim" {...a11yProps(6)} />
+        <Tab label="Intercession" {...a11yProps(6)} />
+        <Tab label="Evangelisation" {...a11yProps(6)} />
+        <Tab label="Technique" {...a11yProps(6)} />
+        <Tab label="Décoration" {...a11yProps(6)} />
+        <Tab label="Nettoyage" {...a11yProps(6)} />
+        <Tab label="Protocole" {...a11yProps(6)} />
+        <Tab label="Chorale" {...a11yProps(6)} />
       </Tabs>
       <TabPanel value={value} index={0} className="w-full">
         <Membres />
