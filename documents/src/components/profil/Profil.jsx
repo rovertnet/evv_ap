43143af9fdcf560a21074e92530ee74094f87037
Robert Matundu <matundukabamba@gmail.com>
2024-10-12 16:53:03 +0100

    another commit

--- documents/src/components/profil/Profil.jsx
+++ documents/src/components/profil/Profil.jsx
@@ -1,19 +1,28 @@
 import profilimg from "../../assets/logo/moi.png"
+import { FaEdit } from "react-icons/fa";
+
 
 export default function Profil() {
   return (
     <>
       <div className="">
-        <div className="h-44 w-full bg-white rounded-lg"></div>
+        <div className="flex justify-end h-44 w-full bg-white rounded-lg space-x-2 p-2">
+          <FaEdit className="text-2xl font-bold text-gray-500" />
+        </div>
         <div className="flex justify-start gap-3">
-          <div className="bg-gray-600 rounded-full py-2 px-2 w-28 h-28 -mt-14 mx-10">
+          <div className=" bg-gray-400 rounded-full py-2 px-2 w-28 h-28 -mt-14 mx-10">
             <img
               src={profilimg}
               alt={profilimg}
-              className="rounded-full w-24 h-24 object-cover"
+              className="rounded-full w-24 h-24 object-cover border-4 border-white "
             />
           </div>
-          <h2 className="flex flex-col gap-0 font-bold text-xl text-slate-800 pt-2">Robert Matundu <span className="font-normal text-gray-500 text-base">Développeur Web</span></h2>
+          <h2 className="flex flex-col gap-0 font-bold text-xl text-slate-800 pt-2">
+            Robert Matundu{" "}
+            <span className="font-normal text-gray-500 text-base">
+              Développeur Web
+            </span>
+          </h2>
         </div>
       </div>
     </>
