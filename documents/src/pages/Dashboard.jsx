fa5cc6c5f7bddee8defbb8da8e525db2d0b2ec3a
Robert Matundu <matundukabamba@gmail.com>
2024-10-20 15:35:31 +0100

    another commit

--- documents/src/pages/Dashboard.jsx
+++ documents/src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import Table from "../components/docs/Table";
 import Count from "../components/TB/Count";
-import SwipeableTemporaryDrawer from "../components/util/Drawer";
 
 
 export default function Dashboard() {
@@ -10,7 +9,6 @@ export default function Dashboard() {
 
       <div className="pt-10">
         <Table />
-        <SwipeableTemporaryDrawer />
       </div>
     </div>
   );
