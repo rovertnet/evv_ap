44ce998b3cda9ceba6836a686ecf27a44b744ec6
Robert Matundu <matundukabamba@gmail.com>
2024-10-24 17:08:05 +0100

    another commit

---
+++ documents/src/components/depart/departs/TablePast.jsx
@@ -0,0 +1,15 @@
+
+
+export default function TablePast() {
+  return (
+    <>
+      <div className="bg-white w-full px-5 py-3">
+        <table>
+          <thead>
+            <th></th>
+          </thead>
+        </table>
+      </div>
+    </>
+  )
+}
