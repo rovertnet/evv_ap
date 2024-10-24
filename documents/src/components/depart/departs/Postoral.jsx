ec1536e738ed92c9a08797fbd6b314df66e2d8f6
Robert Matundu <matundukabamba@gmail.com>
2024-10-24 15:52:53 +0100

    another commit

--- documents/src/components/depart/departs/Postoral.jsx
+++ documents/src/components/depart/departs/Postoral.jsx
@@ -7,12 +7,12 @@ export default function Postoral() {
     <>
       <div className="bg-white py-5 px-5 rounded-none">
         <div className="flex justify-between items-center">
-          <h2 className="font-bold text-xl text-blue-500">
+          <h2 className="font-bold text-xl text-slate-700">
             Membres de Pastoral
           </h2>
-          <Button className="py-3 px-5 bg-blue-500 hover:bg-blue-500 flex space-x-3 rounded-md">
-            <FaPlus />
-            <sppan className="font-semibold text-lg text-white addpast">Ajouter</sppan>
+          <Button className="py-3 px-5 flex space-x-3 rounded-md addpast">
+            <FaPlus className="text-white text-xl" />
+            <sppan className="font-semibold text-lg text-white">Ajouter</sppan>
           </Button>
         </div>
       </div>
