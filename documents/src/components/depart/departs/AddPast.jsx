7b6c48690a4d1e741bb74e74b574648da4816035
Robert Matundu <matundukabamba@gmail.com>
2024-11-02 21:29:15 +0100

    another commit

--- documents/src/components/depart/departs/AddPast.jsx
+++ documents/src/components/depart/departs/AddPast.jsx
@@ -1,16 +1,17 @@
-
+import { FaEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function AddPast() {
   return (
     <>
       <div className="bg-white px-5 py-5 rounded-md">
         <div className="flex justify-between items-center">
-          <h1 className="font-extrabold text-2xl text-blue-700">
-            Entrez les informations
-          </h1>
-          <span className="flex space-x-2">
-            
-          </span>
+          <h2 className="font-bold text-xl text-slate-700">
+            Ajouter un membre
+          </h2>
+          <Link to="/depart/past">
+            <FaEye className="text-2xl font-bold text-slate-900" />
+          </Link>
         </div>
       </div>
     </>
