e1dddcf5664b6fe71e0f023486ed185211c3ce24
Robert Matundu <matundukabamba@gmail.com>
2024-11-02 21:13:31 +0100

    another commit

--- documents/src/components/depart/departs/AddPast.jsx
+++ documents/src/components/depart/departs/AddPast.jsx
@@ -4,8 +4,15 @@ export default function AddPast() {
   return (
     <>
       <div className="bg-white px-5 py-5 rounded-md">
-        <h1 className="font-extrabold text-2xl text-blue-700">Entrez les informations</h1>
+        <div className="flex justify-between items-center">
+          <h1 className="font-extrabold text-2xl text-blue-700">
+            Entrez les informations
+          </h1>
+          <span className="flex space-x-2">
+            
+          </span>
+        </div>
       </div>
     </>
-  )
+  );
 }
