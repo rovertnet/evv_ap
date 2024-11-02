76584402484a9671fbe8bc75de23738bb2704026
Robert Matundu <matundukabamba@gmail.com>
2024-11-02 21:11:39 +0100

    another commit

--- documents/src/components/depart/departs/AddPast.jsx
+++ documents/src/components/depart/departs/AddPast.jsx
@@ -3,7 +3,9 @@
 export default function AddPast() {
   return (
     <>
-      
+      <div className="bg-white px-5 py-5 rounded-md">
+        <h1 className="font-extrabold text-2xl text-blue-700">Entrez les informations</h1>
+      </div>
     </>
   )
 }
