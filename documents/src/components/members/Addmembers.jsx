3290d9f24050fe3ed7451865afe4fd0bb0ddf766
Robert Matundu <matundukabamba@gmail.com>
2024-11-11 13:39:17 +0100

    another commit

--- documents/src/components/members/Addmembers.jsx
+++ documents/src/components/members/Addmembers.jsx
@@ -11,7 +11,7 @@ export default function Addmembers() {
           <h2 className="font-bold text-xl text-slate-700">
             Ajouter un membre
           </h2>
-          <Link to="/depart/past">
+          <Link to="/depart/">
             <FaEye className="text-2xl font-bold text-slate-900" />
           </Link>
         </div>
