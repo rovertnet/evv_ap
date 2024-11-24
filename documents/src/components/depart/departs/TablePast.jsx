7edd784e409b9d3fa8fa788b1f303f9acac7b4ab
Robert Matundu <matundukabamba@gmail.com>
2024-11-24 15:33:15 +0100

    last commit

--- documents/src/components/depart/departs/TablePast.jsx
+++ documents/src/components/depart/departs/TablePast.jsx
@@ -1,7 +1,3 @@
-import photo1 from "../../../assets/logo/RP1.jpeg";
-import { IoEyeOutline } from "react-icons/io5";
-import { GrEdit } from "react-icons/gr";
-import { RiDeleteBinLine } from "react-icons/ri";
 
 
 export default function TablePast() {
@@ -22,31 +18,6 @@ export default function TablePast() {
               <th>Options</th>
             </tr>
           </thead>
-
-          {/* le corps du tableau */}
-          <tbody className="">
-            <tr>
-              <td>01</td>
-              <td>
-                <div className="flex items-center gap-5">
-                  <div className="imgWrapper overflow-hidden w-12 h-12 rounded-full">
-                    <img src={photo1} alt={photo1} />
-                  </div>
-                </div>
-              </td>
-              <td>Matundu</td>
-              <td>Kabamba</td>
-              <td>Robert</td>
-              <td>M</td>
-              <td>+243998724266</td>
-              <td>Ass.Sec</td>
-              <td className="flex space-x-2 text-2xl font-bold">
-                <IoEyeOutline className="text-blue-500" />
-                <GrEdit className="text-green-500" />
-                <RiDeleteBinLine className="text-red-500" />
-              </td>
-            </tr>
-          </tbody>
         </table>
       </div>
     </>
