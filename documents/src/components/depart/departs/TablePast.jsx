ad0b1c31b57c5af92c7cd470c4ed5de5b3e60cd8
Robert Matundu <matundukabamba@gmail.com>
2024-11-24 16:39:53 +0100

    last commit

--- documents/src/components/depart/departs/TablePast.jsx
+++ documents/src/components/depart/departs/TablePast.jsx
@@ -3,7 +3,7 @@
 export default function TablePast() {
   return (
     <>
-      <div className="cards">
+      <div className="cards my-4 border-0">
         <div className="table-responsive">
           <table className="table w-[100%] table-striped">
             <thead>
